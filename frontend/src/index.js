b74c26104bd50c65cd50d3c8c71ae51338b77ddc
DnZmfr <drop5u@yahoo.com>
2021-05-10 20:03:41 +0200

    Update frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ if (!backendURL) {
   try {
     const Application = await getApplication({
       name: 'Conduit',
-      description: 'A place to share your knowledge. New update.',
+      description: 'A place to share your knowledge. v2',
       backendURL
     });
 
