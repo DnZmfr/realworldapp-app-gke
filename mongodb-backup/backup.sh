5c07ed0a578584eaea6947a19eff6fe0b981668a
DnZmfr <drop5u@yahoo.com>
2021-05-12 22:47:41 +0200

    Debug backup

--- mongodb-backup/backup.sh
+++ mongodb-backup/backup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -vx
 
 bold() {
   echo ". $(tput bold)" "$*" "$(tput sgr0)";
@@ -9,6 +10,7 @@ CURRENT_DT=$(date +%Y%m%d_%H%M)
 BACKUP_FILENAME=mongodb_backup_${CURRENT_DT}.gz
 
 bold "Back-up mongoDB database to ${BACKUP_FILENAME}"
+bold "MONGODB_URI is ${MONGODB_URI}"
 mongodump --oplog --archive=${BACKUP_FILENAME} --gzip --uri ${MONGODB_URI}
 
 bold "Copy backup file to Google Cloud Storage..."
