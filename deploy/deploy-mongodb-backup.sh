4f61645e45ce5a19c7324c6dbbba04a4c161e6a7
DnZmfr <7255778+DnZmfr@users.noreply.github.com>
2021-05-13 19:43:09 +0000

    Update mongodb backup script

--- deploy/deploy-mongodb-backup.sh
+++ deploy/deploy-mongodb-backup.sh
@@ -1,18 +1,62 @@
 #!/usr/bin/env bash
-# creates the mongodb-backup cronjob
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-bold() {
+function bold() {
   echo ". $(tput bold)" "$*" "$(tput sgr0)";
 }
 
-bold "Build new mongodb-backup docker image"
-docker build -t gcr.io/${GOOGLE_CLOUD_PROJECT}/mongodb-backup:latest ../mongodb-backup/
-bold "Push the new image to Google Container Registry"
-docker push gcr.io/${GOOGLE_CLOUD_PROJECT}/mongodb-backup:latest
+function show_usage() {
+  echo -e "This script builds a mongodb-backup docker image and deploys the k8s cronjon.\n
+  ERROR: command name argument is expected.\n
+  Available commands:
+  build     builds and deploy a new container image
+  deploy    deploy only"
+  echo -e "\nUsage: $0 <build|deploy>\n"
+}
 
-bold "Create gcp-key secret"
-kubectl create secret generic gcp-key --from-file=service-account.json=/home/zamfidan/.google/realworld-tf-gke.json
-bold "Deploy k8s cronjob"
-kubectl create -f mongodb-cronjob.yaml
+function build {
+  bold "Build new mongodb-backup docker image"
+  docker build -t gcr.io/${GOOGLE_CLOUD_PROJECT}/mongodb-backup:latest ../mongodb-backup/
+
+  bold "Push the new image to Google Container Registry"
+  docker push gcr.io/${GOOGLE_CLOUD_PROJECT}/mongodb-backup:latest
+}
+
+function deploy {
+  bold "Generate kubeconfig"
+  rm -f ${HOME}/.kube/config
+  gcloud container clusters get-credentials realworld-cluster --zone us-central1-c --project ${GOOGLE_CLOUD_PROJECT}
+
+  bold "Create gcp-key secret"
+  kubectl create secret generic gcp-key --from-file=service-account.json=${HOME}/.google/realworld-tf-gke.json
+
+  bold "Deploy k8s cronjob"
+  kubectl apply -f mongodb-cronjob.yaml
+}
+
+function process_args() {
+  while [[ $# > 0 ]]; do
+    local arg="$1"
+    shift
+    case $arg in
+      build)
+        build
+        deploy
+        ;;
+      deploy)
+        deploy
+        ;;
+      *)
+        echo -e "\nUnrecognized argument '$arg'.\n"
+        show_usage
+        exit -1
+    esac
+  done
+}
+
+if [ $# -eq 0 ]; then
+  show_usage
+else
+  process_args "$@"
+fi
